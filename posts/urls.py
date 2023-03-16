2116e33d024be6c559baf32f710ec74aed2eb523
Praise Idowu <ifeoluwapraise02@gmail.com>
2023-03-16 13:12:26 +0100

    Implemented viewsets and routers

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from . views import PostList, PostDetail, UserList, UserDetail
+from rest_framework.routers import SimpleRouter
+from . views import PostList, PostDetail, UserList, UserDetail, UserViewset, PostViewset
 
-urlpatterns = [
-    path('users/', UserList.as_view()),
-    path('users/<int:pk>/', UserDetail.as_view()),
-    path('<int:pk>/', PostDetail.as_view()),
-    path('', PostList.as_view()),
-]
+# urlpatterns = [
+#     path('users/', UserList.as_view()),
+#     path('users/<int:pk>/', UserDetail.as_view()),
+#     path('<int:pk>/', PostDetail.as_view()),
+#     path('', PostList.as_view()),
+# ]
+
+router = SimpleRouter()
+
+router.register('users', UserViewset, basename='users')
+router.register('', PostViewset, basename='post')
+
+urlpatterns = router.urls
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
 from rest_framework import generics, permissions
+from rest_framework import viewsets
 from . models import Post      
 from . serializers import PostSerializer, UserSerializer
 from . permissions import IsAuthorOrReadOnly
@@ -20,5 +21,14 @@ class UserList(generics.ListCreateAPIView):
     serializer_class = UserSerializer
     
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+class PostViewset(viewsets.ModelViewSet):  # provides list view and detail view for us
+    permission_classes = [IsAuthorOrReadOnly]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
+class UserViewset(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
